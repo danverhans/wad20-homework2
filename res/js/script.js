9062f505dd56793f52077f7bb992d04d5ec6f20f
danverhans <danver.varv@gmail.com>
2020-10-18 13:16:04 +0300

    first task almost done

---
+++ res/js/script.js
@@ -0,0 +1,33 @@
+$(function(){
+  
+    $('.avatar').click(function () {
+        $('#menu').toggle()
+    })
+
+    loadUserInfo().then(function (response) {
+        displayUserInfo(response)
+    })
+    .catch(function () {
+        alert('Error loading user info')
+    });
+
+});
+
+function loadUserInfo() {
+    return $.get(
+        {
+            url: 'https://private-anon-609e31d177-wad20postit.apiary-mock.com/users/1',
+            success: function (response) {
+                return response;
+            },
+            error: function () {
+                alert('error')
+            }
+        }
+    );
+}
+
+function displayUserInfo(response) {
+    $('#name').text(response.firstname + " " + response.lastname);
+    $('#email').text(response.email);
+}
